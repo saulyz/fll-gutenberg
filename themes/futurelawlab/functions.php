4bc8f2544f3b4baacbe7d5a24c9cb10fa18c870a
Saulius Vikerta <saulius.vikerta@gmail.com>
2020-09-09 21:55:27 +0300

    adding typography styles for editor blocks

--- themes/futurelawlab/functions.php
+++ themes/futurelawlab/functions.php
@@ -14,7 +14,14 @@ function fll_webfonts() {
     wp_enqueue_style( 
       'futurelawlab-fonts', 
       'https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;600&display=swap'
-    ); 
+    );
 }
 
 add_action( 'wp_enqueue_scripts', 'fll_webfonts' ); 
+add_action( 'enqueue_block_editor_assets', 'fll_webfonts', 1, 1 );
+
+function fll_block_editor_styles() {
+	wp_enqueue_style( 'futurelawlab-block-editor-styles', get_stylesheet_directory_uri() . '/editor-style-block.css', ['futurelawlab-fonts'] );
+}
+
+add_action( 'enqueue_block_editor_assets', 'fll_block_editor_styles', 1, 1 );
